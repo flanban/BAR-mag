ee3d3a7ee9524d30fd512e661d20e33bbb14e793
flanban <johnsfuller@gmail.com>
2013-12-20 16:00:24 -0500

    tweak button styles

--- public/javascripts/thumbnail-nav.js
+++ public/javascripts/thumbnail-nav.js
@@ -4,7 +4,10 @@ $(window).load(function() {
         $("#thumbnail-toggle").text($(this).is(':visible') ? "Hide" : "Show");
       });
   });
-  var toggleBtnWidth = $('#thumbnail-toggle').width();
-  var toggleBtnOffset = toggleBtnWidth/2;
-  $('#thumbnail-toggle').css('margin-left', - toggleBtnOffset);
+  function initButton() {
+    var toggleBtnWidth = $('#thumbnail-toggle').width();
+    var toggleBtnOffset = toggleBtnWidth/2;
+    $('#thumbnail-toggle').css('margin-left', - toggleBtnOffset);
+  }
+  initButton();
 });
