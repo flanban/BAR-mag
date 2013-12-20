e3ac311990929d31b2a1d3023c975c9ab7bcb84c
flanban <johnsfuller@gmail.com>
2013-12-20 17:24:47 -0500

    remove unused files

---
+++ public/javascripts/thumbnails.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+  $('#thumbnail-toggle').click(function(){
+    $('#thumb-nav-wrap').slideToggle(function() {
+        $("#menu-toggle").text($(this).is(':visible') ? "Hide" : "Show");
+      });
+  });
+  function initButton() {
+    var toggleBtnWidth = $('#thumbnail-toggle').width();
+    var toggleBtnOffset = toggleBtnWidth/2;
+    $('#thumbnail-toggle').css('margin-left', - toggleBtnOffset);
+  }
+  initButton();
+});
