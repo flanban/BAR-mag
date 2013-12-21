96f682eb6f6056cd34dbd121c0da1547b3f2a321
flanban <johnsfuller@gmail.com>
2013-12-20 19:12:05 -0500

    removing unused script

--- public/javascripts/thumbnails.js
+++ public/javascripts/thumbnails.js
@@ -1,12 +1,15 @@
 $(window).load(function() {
   $('#menu-toggle').click(function() {
-      $("#menu-toggle").text($(this).prev().is(':visible') ? "Hide" : "Show")
       var $lefty = $(this).parent();
-      
       $lefty.animate({
-        left: parseInt($lefty.css('left'),10) == 0 ?
+        left: [parseInt($lefty.css('left'),10) == 0 ?
           -$lefty.outerWidth() :
-          0
+          0],
       });
     }); 
+    
+    $("#menu-toggle").text($(this).next().is(':visible') ? "hide" : "show")
+    
+    
+    
 });
